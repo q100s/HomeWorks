e37e601a352c12f78472b9e12460924bc37d503e
eduard <eduard42rus@mail.ru>
2023-03-24 22:44:45 +0700

    Lesson2 is done

--- src/pro/sky/java/course1/lesson1/Author.java
+++ src/pro/sky/java/course1/lesson1/Author.java
@@ -3,16 +3,34 @@ package pro.sky.java.course1.lesson1;
 public class Author {
     private String firstName;
     private String secondName;
-    public Author(String firstName, String secondName){
+
+    public Author(String firstName, String secondName) {
         this.firstName = firstName;
         this.secondName = secondName;
     }
+
     public Author() {
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getSecondName() {
         return secondName;
+    }
+
+    public String toString() {
+        return " by " + this.firstName + " " + this.secondName;
+    }
+
+    public boolean equals(Author other) {
+        if (this.firstName.equals(other.firstName) && this.secondName.equals(other.secondName)) {
+            return true;
+        }
+        return false;
+    }
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, secondName);
     }
 }
--- src/pro/sky/java/course1/lesson1/Book.java
+++ src/pro/sky/java/course1/lesson1/Book.java
@@ -2,23 +2,44 @@ package pro.sky.java.course1.lesson1;
 
 public class Book {
     private String nameOfBook;
-    private Author author = new Author();
+    private Author author;
     private int publishingYear;
+
     public Book(String nameOfBook, Author author, int publishingYear) {
         this.nameOfBook = nameOfBook;
         this.author = author;
         this.publishingYear = publishingYear;
     }
+
     public String getNameOfBook() {
         return nameOfBook;
     }
+
     public Author getAuthor() {
         return author;
     }
+
     public int getPublishingYear() {
         return publishingYear;
     }
+
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
+
+    public String toString() {
+        return "Name of book is " + this.nameOfBook + author + " in " + this.publishingYear;
+    }
+
+    public boolean equals(Book other) {
+        if (this.nameOfBook.equals(other.nameOfBook) && this.author.equals(other.author) && this.publishingYear == other.publishingYear) {
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(nameOfBook, author, publishingYear);
+    }
 }
+
